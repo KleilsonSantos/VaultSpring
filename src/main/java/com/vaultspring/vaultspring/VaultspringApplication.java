47187e9eab912369bef3b90e22c2da8c51ee9407
Kleilson Santos <kdsdesign1@gmail.com>
2025-06-10 16:38:31 -0300

    ✨ Add initial project structure with Maven configuration, application code, and documentation

---
+++ src/main/java/com/vaultspring/vaultspring/VaultspringApplication.java
@@ -0,0 +1,11 @@
+package com.vaultspring.vaultspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultspringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VaultspringApplication.class, args);
+    }
+}
---
+++ src/test/java/com/vaultspring/vaultspring/VaultspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaultspring.vaultspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
